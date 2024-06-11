e17b9e56eb6c46f7052c9c9c3406d08f932f2d1d
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-06-11 18:03:51 +0300

    add new files

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 
 #создаёт экземпляр класса Flask (переменную app)
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'your_will-never-guess'
 
 from app import routes
